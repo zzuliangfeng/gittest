7bce8222dc049e0718e6c72e82b11a2c1a4c01dd
zzuliangfeng <48082459+zzuliangfeng@users.noreply.github.com>
2022-05-11 09:28:23 +0800

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -4,5 +4,6 @@ public class hello {
         System.out.println("hello, world2");
         System.out.println("hello, master");
         System.out.println("hello, hot-fix");
+        System.out.println("edited by github");
     }
 }
