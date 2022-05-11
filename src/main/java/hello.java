cdd9ce1492270869e37ccf532040890db461de14
zzuliangfeng <898942868@qq.com>
2022-05-11 09:16:53 +0800

    master third commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello, world");
         System.out.println("hello, world2");
+        System.out.println("hello, master");
     }
 }
